280d9fca4f5911895cdee8f4f82905a8638c0911
Alexander <roy.alex.0708@gmail.com>
2026-05-06 19:29:29 +0500

    1

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
         prefix: '/uploads/',
     });
 
-    await app.listen(process.env.PORT ?? 3000);
+    await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap().then();
